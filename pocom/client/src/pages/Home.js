252f9c3154381ac9dee9d2c19a9cf49cd7576dc2
mauritalkie <93022536+mauritalkie@users.noreply.github.com>
2023-04-09 23:06:47 -0600

    only html by now

--- pocom/client/src/pages/Home.js
+++ pocom/client/src/pages/Home.js
@@ -3,8 +3,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => { 
     return (
-      <div>
-        <p>HOME</p>
+      <div className="text-white">
+        <div>
+          <h1>Your feed</h1>
+        </div>
+
+        <div>
+          <p>community</p>
+          <p>published by</p>
+          <button>join comunity</button>
+        </div>
+
+        <h3>Topic post</h3>
+
+        <div>
+          <p>Post content</p>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/07/Reddit_icon.svg"></img>
+        </div>
+
+        <div>
+          <button>Like</button>
+          <button>Dislike</button>
+          <p>comments number</p>
+        </div>
+
+        <div>
+          <p>make a comment</p>
+          <input type="text"></input>
+        </div>
       </div>
     );
   }
